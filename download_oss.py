326de4eb4b7a0dacaa376c51f6776e3725d403f6
travisergodic <travisergodic1993@gmail.com>
2025-08-07 21:18:48 +0800

    revise

--- download_oss.py
+++ download_oss.py
@@ -1,3 +1,4 @@
+import os
 import oss2
 
 # ✅ 填入你的配置
@@ -6,13 +7,19 @@ access_key_secret = ''
 bucket_name = 'travis-hf-model'
 endpoint = 'oss-cn-hangzhou.aliyuncs.com' 
 oss_key = 'qwen25-vl/Qwen2.5-VL-7B-Instruct.tar'  # OSS 上的路径
-local_file = '/home/bglab/htw/Qwen2.5-VL-7B-Instruct.tar'
+local_dir = ''
 
 # 初始化认证与 bucket
 auth = oss2.Auth(access_key_id, access_key_secret)
 bucket = oss2.Bucket(auth, endpoint, bucket_name)
 
 # 下载
-bucket.get_object_to_file(oss_key, local_file)
+for oss_key in (
+    'qwen25-vl/Qwen2.5-VL-7B-Instruct.tar', 
+    'qwen3/models--Qwen--Qwen3-Embedding-0.6B.tar',
+    'qwen3/models--Qwen--Qwen3-Reranker-0.6B.tar'
+):
+    local_file = os.path.join(local_dir, os.path.basename(oss_key))
+    bucket.get_object_to_file(oss_key, local_file)
 
-print(f"✅ 下载成功: {oss_key} -> {local_file}")
+    print(f"✅ 下载成功: {oss_key} -> {local_file}")
